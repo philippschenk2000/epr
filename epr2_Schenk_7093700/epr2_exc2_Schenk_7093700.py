0b1ed2b5c8c54c505b79e6693adb005cc0a480a2
philippschenk <brv.vierer@gmail.com>
2023-10-26 22:42:46 +0200

    newf

--- epr2_Schenk_7093700/epr2_exc2_Schenk_7093700.py
+++ epr2_Schenk_7093700/epr2_exc2_Schenk_7093700.py
@@ -2,24 +2,26 @@ def main():
     __author__ = "7093700, Schenk"
     try:
         # INPUT OF NUMBERS
-        a = int(input('Enter first number (range 0 - 110) for GPR: '))
-        b = int(input('Enter second number (range 0 - 110) for EPR: '))
-        c = int(input('Enter third number for ZBNP: '))
-        if 0 <= a <= 110 and 0 <= b <= 110:
-            print(0)
+        gpr = int(input('Enter points for GPR (range 0 - 110): '))
+        epr = int(input('Enter points for EPR (range 0 - 110): '))
+        zbnp = int(input('Enter third number for ZBNP: '))
+        # CHECK FOR RANGE
+        if 0 <= gpr <= 110 and 0 <= epr <= 110:
+            bonus_points = calc_bonus_points(gpr, epr, zbnp)
+            returning(bonus_points)
         else:
             print('First number and second number have to be between 0 and 110')
     except:
-        print('Input has to be a integer.')
+        print('All input has to be integer.')
 
 
-def calc_harmonic_mid(a, b):
-    return 2*a*b/(a+b)
+# BONUS POINTS CALCULATION
+def calc_bonus_points(gpr, epr, zbnp):
+    return round(min(zbnp / 4, (epr + gpr) / 14), 2)
 
 
-def returning(harmonic_mid, arithmetic_mid):
-    print(f'The harmonic mean is: {harmonic_mid}')
-    print(f'The arithmetic mean is: {arithmetic_mid}')
+def returning(bonus_points):
+    print(f'The bonus points for the input would be {bonus_points} points')
 
 
 main()
--- epr2_Schenk_7093700/epr2_exc3_Schenk_7093700.py
+++ epr2_Schenk_7093700/epr2_exc3_Schenk_7093700.py
@@ -1,27 +1,19 @@
 def main():
     __author__ = "7093700, Schenk"
-    # INPUT OF NUMBERS
     try:
-        a = float(input('Enter first number: '))
-        b = float(input('Enter second number: '))
-        # NOW CONDUCT TO EACH PART & BE AWARE OF DIVING WITH ZERO
-        arithmetic_mid = calc_arithmetic_mid(a, b)
-        if arithmetic_mid == 0:
-            returning('Because the sum of both is zero, the harmonic mean can not be calculated', arithmetic_mid)
-        else:
-            harmonic_mid = calc_harmonic_mid(a, b)
-            returning(harmonic_mid, arithmetic_mid)
+        # INPUT OF NUMBERS
+        a = int(input('Enter first number: '))
+        b = int(input('Enter second number: '))
+        # CALC MINIMUM
+        minimum = min(a, b)
+        for elem in [2, 4, 8]:
+            rest = minimum % elem
+            if rest == 0:
+                print(f'The minimum of both numbers ({minimum}) is dividable by {elem} with no rest')
+            else:
+                print(f'The minimum of both numbers ({minimum}) is NOT dividable by {elem} with a rest of {rest}')
     except:
-        print('Input has to be a float or integer.')
-
-
-def calc_harmonic_mid(a, b):
-    return 2*a*b/(a+b)
-
-
-def returning(harmonic_mid, arithmetic_mid):
-    print(f'The harmonic mean is: {harmonic_mid}')
-    print(f'The arithmetic mean is: {arithmetic_mid}')
+        print('All input has to be integer.')
 
 
 main()
