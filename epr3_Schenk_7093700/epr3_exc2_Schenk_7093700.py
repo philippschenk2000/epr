9833d47ba930caa7fb8c106d1f5fbc37234b34ce
philippschenk <brv.vierer@gmail.com>
2023-11-02 21:03:35 +0100

    newf

---
+++ epr3_Schenk_7093700/epr3_exc2_Schenk_7093700.py
@@ -0,0 +1,5 @@
+def f3(n):
+    return n+a
+a = 4
+y = f3(6)
+print(y)
---
+++ epr3_Schenk_7093700/epr3_exc3_Schenk_7093700.py
@@ -0,0 +1,21 @@
+def main():
+    __author__ = "7093700, Schenk"
+    try:
+        # INPUT OF NUMBERS
+        print('The program will calculate the rest after dividing the minimum of both by 2, 4 & 8.')
+        a = int(input('Enter first number: '))
+        b = int(input('Enter second number: '))
+        # CALC MINIMUM
+        minimum = min(a, b)
+        for elem in [2, 4, 8]:
+            # CALC REST
+            rest = minimum % elem
+            if rest == 0:
+                print(f'The minimum of both numbers ({minimum}) is dividable by {elem} with no rest')
+            else:
+                print(f'The minimum of both numbers ({minimum}) is NOT dividable by {elem} with a rest of {rest}')
+    except:
+        print('All input has to be non-decimal number.')
+
+
+main()
