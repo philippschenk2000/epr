18aa2acebb826fd1e392202756695bf8bdfded82
philippschenk <brv.vierer@gmail.com>
2023-11-07 18:45:07 +0100

    newf

--- epr3_Schenk_7093700/epr3_exc2_Schenk_7093700.py
+++ epr3_Schenk_7093700/epr3_exc2_Schenk_7093700.py
@@ -1,3 +1,5 @@
+__author__ = "7093700, Schenk"
+
 def function_a(a, b):
     # TRY EXCEPT FOR NON INTEGER NUMBERS
     try:
@@ -67,35 +69,110 @@ def catalan_constant_d_e(a, b, steps):
 
 
 f_a = function_a(input('First number: '), input('Second number: '))
+print(f_a)
 # TEST 1:
-# IN:       SHOULD BE:      OUT:
+# IN: -12.0, 20       SHOULD BE: Both inputs have to be a number.     OUT: Both inputs have to be a number.
 # TEST 2:
-# IN:       SHOULD BE:      OUT:
+# IN: -12, 20       SHOULD BE: 132     OUT: 132
 # TEST 3:
-# IN:       SHOULD BE:      OUT:
+# IN: 2, 489      SHOULD BE: 119804     OUT: 119804
 
-f_b = function_b(12)
-
+f_b = function_b(389270.578)
+print(f_b)
 # TEST 1:
-# IN:       SHOULD BE:      OUT:
+# IN: 12      SHOULD BE: 1079     OUT: 1079
 # TEST 2:
-# IN:       SHOULD BE:      OUT:
+# IN: 2      SHOULD BE: 1076     OUT: 1076
 # TEST 3:
-# IN:       SHOULD BE:      OUT:
+# IN: 389270.578      SHOULD BE: 1094     OUT: 1094
 
-f_c = function_c(1)
+f_c = function_c(2)
 # TEST 1:
-# IN:       SHOULD BE:      OUT:
-# TEST 2:
-# IN:       SHOULD BE:      OUT:
-# TEST 3:
-# IN:       SHOULD BE:      OUT:
+# IN: 1      SHOULD BE: 0     OUT: 0
 
-f_d_e = catalan_constant_d_e(4, 10, 3)
-# TEST 1:
-# IN:       SHOULD BE:      OUT:
 # TEST 2:
-# IN:       SHOULD BE:      OUT:
+# IN: 6
+# SHOULD BE:
+# 0 1 0 1 0 1
+# 1 0 1 0 1 0
+# 0 1 0 1 0 1
+# 1 0 1 0 1 0
+# 0 1 0 1 0 1
+# 1 0 1 0 1 0
+# OUT:
+# 0 1 0 1 0 1
+# 1 0 1 0 1 0
+# 0 1 0 1 0 1
+# 1 0 1 0 1 0
+# 0 1 0 1 0 1
+# 1 0 1 0 1 0
+
 # TEST 3:
-# IN:       SHOULD BE:      OUT:
+# IN: 2
+# SHOULD BE:
+# 0 1
+# 1 0
+# OUT:
+# 0 1
+# 1 0
+
+f_d_e = catalan_constant_d_e(4, 5, 1)
+# TEST 1:
+# IN: 4, 10, 3
+# SHOULD BE:
+# n = 4  Catalan constant is:  0.9084807256235827
+# n = 7  Catalan constant is:  0.9184791015893248
+# Difference is:  0.009998375965742046
+# OUT:
+# n = 4  Catalan constant is:  0.9084807256235827
+# n = 7  Catalan constant is:  0.9184791015893248
+# Difference is:  0.009998375965742046
+
+# TEST 2:
+# IN: 4, 23, 2
+# SHOULD BE:
+# n = 4  Catalan constant is:  0.9084807256235827
+# n = 6  Catalan constant is:  0.9125619418260111
+# Difference is:  0.004081216202428406
+# n = 8  Catalan constant is:  0.9140346571448803
+# Difference is:  0.0014727153188691666
+# n = 10  Catalan constant is:  0.914724781654844
+# Difference is:  0.0006901245099637521
+# n = 12  Catalan constant is:  0.9151019961827472
+# Difference is:  0.0003772145279031802
+# n = 14  Catalan constant is:  0.9153302540702645
+# Difference is:  0.00022825788751723586
+# n = 16  Catalan constant is:  0.9154787319860305
+# Difference is:  0.00014847791576599878
+# n = 18  Catalan constant is:  0.9155806791009646
+# Difference is:  0.00010194711493416442
+# n = 20  Catalan constant is:  0.9156536770949605
+# Difference is:  7.299799399584916e-05
+# n = 22  Catalan constant is:  0.9157077282099416
+# Difference is:  5.405111498113513e-05
+# OUT:
+# n = 4  Catalan constant is:  0.9084807256235827
+# n = 6  Catalan constant is:  0.9125619418260111
+# Difference is:  0.004081216202428406
+# n = 8  Catalan constant is:  0.9140346571448803
+# Difference is:  0.0014727153188691666
+# n = 10  Catalan constant is:  0.914724781654844
+# Difference is:  0.0006901245099637521
+# n = 12  Catalan constant is:  0.9151019961827472
+# Difference is:  0.0003772145279031802
+# n = 14  Catalan constant is:  0.9153302540702645
+# Difference is:  0.00022825788751723586
+# n = 16  Catalan constant is:  0.9154787319860305
+# Difference is:  0.00014847791576599878
+# n = 18  Catalan constant is:  0.9155806791009646
+# Difference is:  0.00010194711493416442
+# n = 20  Catalan constant is:  0.9156536770949605
+# Difference is:  7.299799399584916e-05
+# n = 22  Catalan constant is:  0.9157077282099416
+# Difference is:  5.405111498113513e-05
+
+# TEST 3:
+# IN: 4, 5, 1
+# SHOULD BE: n = 4  Catalan constant is:  0.9084807256235827
+# OUT: n = 4  Catalan constant is:  0.9084807256235827
 
