641f5ee55be9638684cbd1048d18763406243bb7
philippschenk <brv.vierer@gmail.com>
2023-11-07 14:01:45 +0100

    newf

--- epr3_Schenk_7093700/epr3_exc2_Schenk_7093700.py
+++ epr3_Schenk_7093700/epr3_exc2_Schenk_7093700.py
@@ -1,8 +1,11 @@
 def function_a(a, b):
+    # TRY EXCEPT FOR NON INTEGER NUMBERS
     try:
         sum1 = []
+        # PREPARE FOR THE FOR-LOOP BY GOING FROM LOWER TO HIGHER NUMBER
         lower_one = min(int(a), int(b))
         higher_one = max(int(a), int(b))
+        # RANGE SHOULD INCLUDE THE LAST NUMBER TOO
         for i in range(lower_one, higher_one + 1):
             sum1.append(i)
         return sum(sum1)
@@ -11,22 +14,30 @@ def function_a(a, b):
 
 
 def function_b(a):
+    # SET STEPS TO 0 IN CASE a IS 0
     steps = 0
     while a != 0:
         a = a / 2
+        # ADD UP +1 FOR EVERY STEP
         steps = steps + 1
     return steps
 
 
 def function_c(length):
+
+    # PUT 0 IN A LIST IN CASE THE length IS 0
     listed = [0]
+    # CREATE ONE ALTERNATING LIST WITH ONEs and ZEROs
     for l in range(length):
         if listed[-1] == 0:
             listed.append(1)
         else:
             listed.append(0)
+    # CREATE A SECOND ALTERNATING LIST BUT WITH THE OTHER NUMBER AT THE BEGINNING
     listed2 = listed[1:]
+    # KICK OUT THE LAST ELEMENT OF THE FIRST LIST
     listed = listed[:-1]
+    # PRINT BOTH LISTS ALSO ALTERNATING IN THE CONSOLE, WITHOUT COMMAS AND BRACKETS
     for k in range(len(listed)):
         if k % 2 == 0:
             print(str(listed).replace('[', '').replace(']', '').replace(',', ''))
@@ -35,7 +46,9 @@ def function_c(length):
 
 
 def catalan_constant_d_e(a, b, steps):
+    # CREATE AN EMPTY LIST
     listed = []
+    #
     for r in range(a, b, steps):
         listed2 = []
         for n in range(r):
@@ -49,7 +62,7 @@ def catalan_constant_d_e(a, b, steps):
 
 
 #f_a = function_a(input('First number: '), input('Second number: '))
-#f_b = function_b(preferred_number)
-#f_c = function_c(preferred_length)
-#f_d_e = catalan_constant_d_e(4, 10, 3)
+#f_b = function_b(12)
+#f_c = function_c(1)
+f_d_e = catalan_constant_d_e(4, 10, 3)
 
