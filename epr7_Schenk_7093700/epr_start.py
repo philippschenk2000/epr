91dc8a9a54ca49ab0c5d03cd884a97946f587a04
philippschenk <brv.vierer@gmail.com>
2023-12-06 12:48:52 +0100

    newf

--- epr7_Schenk_7093700/epr_start.py
+++ epr7_Schenk_7093700/epr_start.py
@@ -10,10 +10,9 @@ def main():
     # DEFINE GRAPH VIA INPUT
     graphing = [((1, 1), (3, 3)), ((1, 1), (4, 1)), ((7, 7), (4, 2)), ((7, 7), (1, 1))]
     try:
-        #graphing = [eval(input('Enter your edge number ' + str(g) + ' in coordinate format: ((1, 1), (3, 3)): ')) for g in range(int(input('Enter number of edges in your graph: ')))]
+        graphing = [eval(input('Enter your edge number ' + str(g + 1) + ' in coordinate format: ((1, 1), (3, 3)): ')) for g in range(int(input('Enter number of edges in your graph: ')))]
         # TRY: TEST IF THE PROVIDED GRAPH REALLY IS IN CORRECT FORMAT
         tree_status = epr_graph.test_for_tree(graphing)
-        #tree_status = True
         # JUST IF TREE = TRUE: CALL THE LEAVES AND ROOT FUNCTIONS AND PRINT THEM
         if tree_status:
             print('The provided graph is not just a graph but also a tree!')
