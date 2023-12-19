ee3304ede2244cb20271694f098f5a3731d3b621
philippschenk <brv.vierer@gmail.com>
2023-12-19 19:15:33 +0100

    newf

--- epr8_Schenk_7093700/epr_exercise1.py
+++ epr8_Schenk_7093700/epr_exercise1.py
@@ -3,40 +3,74 @@
 __author__ = "7093700, Schenk"
 
 
+def create_borders(matrix, border_value):
+    """ This function creates one lie of borders around our matrix (with value = 100).
+
+    """
+    matrix_with_borders = [[border_value for n in range(len(matrix) + 2)]]
+    for part in matrix:
+        matrix_with_borders.append([border_value] + part + [border_value])
+    matrix_with_borders.append([border_value for n in range(len(matrix) + 2)])
+    return matrix_with_borders
+
+
+def search_for_lightest_way(matrix_with_borders, starting_point):
+    """ This function calls and prints the two functions to walk through the created matrix.
+
+    """
+    # REMEMBER THE PATH FROM NOW ON
+    # CALL THE OTHER FUNCTION ONE TIME WHICH CALLS ITSELF RECURSIVELY
+    ending_point = (len(matrix_with_borders) - 1, len(matrix_with_borders) - 1)
+    if starting_point == ending_point:
+        return [starting_point]
+
+    path = [starting_point]
+    costs = [matrix_with_borders[1][1]]
+    path, costs = walk_the_matrix_locally(matrix_with_borders, starting_point, ending_point, path, costs)
+    print(path, sum(costs))
+
+    best_path = [()]
+    best_cost = [100]
+    optimal_path, optimal_costs = walk_the_matrix_globally(matrix_with_borders, starting_point[0], starting_point[1], [], 0, best_cost, best_path)
+    print(optimal_path, sum(optimal_costs))
+
+
 def walk_the_matrix_locally(matrix_with_borders, starting_point, ending_point, path, costs):
+    """ This function walks the matrix based on the local neighbours.
+
+    """
     # CHANGE THE STARTING POINT AND THE NEIGHBORS FROM HERE ON
     if starting_point == ending_point:
         return path, costs
     else:
-        #print(path, costs)
         right_neighbour = (starting_point[0], starting_point[1] + 1)
         bottom_neighbour = (starting_point[0] + 1, starting_point[1])
         if max(right_neighbour[1], bottom_neighbour[1]) < len(matrix_with_borders) - 1:
             value_right_neighbour = matrix_with_borders[starting_point[0]][starting_point[1] + 1]
             value_bottom_neighbour = matrix_with_borders[starting_point[0] + 1][starting_point[1]]
-            #print(value_right_neighbour, 'vs', value_bottom_neighbour)
             # CASE: VALUE FROM BOTH THE SAME
             if value_right_neighbour < value_bottom_neighbour:
                 path.append(right_neighbour)
                 costs.append(value_right_neighbour)
-                #print('The walk goes from:', starting_point, 'to', ending_point, 'over the point', right_neighbour)
                 walk_the_matrix_locally(matrix_with_borders, right_neighbour, ending_point, path, costs)
             # CASE: VALUE FROM BOTH THE SAME
             elif value_bottom_neighbour < value_right_neighbour:
                 path.append(bottom_neighbour)
                 costs.append(value_bottom_neighbour)
-                #print('The walk goes from:', starting_point, 'to', ending_point, 'over the point', bottom_neighbour)
                 walk_the_matrix_locally(matrix_with_borders, bottom_neighbour, ending_point, path, costs)
             # CASE: VALUE FROM BOTH THE SAME
             else:
                 path.append(right_neighbour)
                 costs.append(value_right_neighbour)
-                #print('The walk goes from:', starting_point, 'to', ending_point, 'over the point', right_neighbour)
                 walk_the_matrix_locally(matrix_with_borders, right_neighbour, ending_point, path, costs)
     return path, costs
 
 
+
 def walk_the_matrix_globally(matrix_with_borders, x, y, path, cost, best_cost, best_path):
+    """ This function walks the matrix in every possible way to get the global optimal path.
+
+    """
     if x >= len(matrix_with_borders) - 1 or y >= len(matrix_with_borders[0]) - 1:
         return  # Außerhalb der Matrixgrenzen
     if (x, y) in path:
@@ -60,3 +94,15 @@ def walk_the_matrix_globally(matrix_with_borders, x, y, path, cost, best_cost, b
     return best_path, best_cost
 
 
+def doc_tests():
+    """ This functions contains the doc tests on the two ways to walk the created matrix.
+    >>> search_for_lightest_way(create_borders([[4, 0], [-1, 7]], 100), (1, 1))
+    [(1, 1), (2, 1), (2, 2)] 10
+    [[(1, 1), (2, 1), (2, 2)]] 10
+    >>> search_for_lightest_way(create_borders([[4, 0, 8], [-3, -4, 7], [-8, -1, 7]], 100), (1, 1))
+    [(1, 1), (2, 1), (3, 1), (3, 2), (3, 3)] -1
+    [[(1, 1), (2, 1), (3, 1), (3, 2), (3, 3)]] -1
+    >>> search_for_lightest_way(create_borders([[4, 0, 8, -4], [-3, -4, 7, -5], [-8, -1, 7, 1], [-4, -4, 6, 1]], 100), (1, 1))
+    [(1, 1), (2, 1), (3, 1), (4, 1), (4, 2), (4, 3), (4, 4)] -8
+    [[(1, 1), (2, 1), (3, 1), (4, 1), (4, 2), (4, 3), (4, 4)]] -8
+    """
--- epr8_Schenk_7093700/epr_timetest.py
+++ epr8_Schenk_7093700/epr_timetest.py
@@ -1,41 +1,20 @@
-""" This script contains different functions for the new exercise in epr """
+""" This script contains the time stopping function for the new exercise in epr """
 
 __author__ = "7093700, Schenk"
 
-import random
+
 import time
 import timeit
 
-import epr_exercise_1
+import epr_exercise1
 
 
-def main():
-    n = 10
-    border_value = 100
-    starting_point = (1, 1)     # because of borders (0, 0) --> (1, 1)
-    matrix = create_matrix(n)
-    matrix_with_borders = create_borders(matrix, border_value)
-    search_for_lightest_way(matrix_with_borders, starting_point)
-
-
-def create_matrix(n):
-    """ This function checks if the provided graph is tree.
+def main(matrix):
+    """ This function stops the time for the two exercises/ two different ways to go through the matrix.
 
     """
-    matrix = [[random.randint(-9, 9) for _ in range(n)] for _ in range(n)]
-    #matrix = [[4, 0, 8], [-3, -4, 7], [-8, -1, 7]]
-    return matrix
-
-
-def create_borders(matrix, border_value):
-    matrix_with_borders = [[border_value for n in range(len(matrix) + 2)]]
-    for part in matrix:
-        matrix_with_borders.append([border_value] + part + [border_value])
-    matrix_with_borders.append([border_value for n in range(len(matrix) + 2)])
-    return matrix_with_borders
-
-
-def search_for_lightest_way(matrix_with_borders, starting_point):
+    starting_point = (1, 1)
+    matrix_with_borders = epr_exercise1.create_borders(matrix, 100)
     print('This is the matrix with borders of value=100:')
     [print(row) for row in matrix_with_borders]
     # REMEMBER THE PATH FROM NOW ON
@@ -49,9 +28,10 @@ def search_for_lightest_way(matrix_with_borders, starting_point):
     print('\n', '-' * 50, '1) This part is for the local optimum', '-' * 50)
     start_time = time.time()
     start_timeit = timeit.timeit()
-    path, costs = epr_exercise_1.walk_the_matrix_locally(matrix_with_borders, starting_point, ending_point, path, costs)
+    path, costs = epr_exercise1.walk_the_matrix_locally(matrix_with_borders, starting_point, ending_point, path, costs)
     end_time = time.time()
     end_timeit = timeit.timeit()
+    print(path, sum(costs))
     print('Needed time (in seconds, with package time):', end_time - start_time)
     print('Needed time (in seconds, with package timeit):', end_timeit - start_timeit)
     print('Result: costs of', sum(costs), 'with the path:', path)
@@ -61,14 +41,21 @@ def search_for_lightest_way(matrix_with_borders, starting_point):
     best_cost = [100]
     start_time = time.time()
     start_timeit = timeit.timeit()
-    optimal_path, optimal_costs = epr_exercise_1.walk_the_matrix_globally(matrix_with_borders, starting_point[0], starting_point[1], [], 0, best_cost, best_path)
+    optimal_path, optimal_costs = epr_exercise1.walk_the_matrix_globally(matrix_with_borders, starting_point[0], starting_point[1], [], 0, best_cost, best_path)
     end_time = time.time()
     end_timeit = timeit.timeit()
+    print(optimal_path, sum(optimal_costs))
     print('Needed time (in seconds, with package time):', end_time - start_time)
     print('Needed time (in seconds, with package timeit):', end_timeit - start_timeit)
-    print('The optimal path for the starting point to go to the end point is:\n', optimal_path)
+    print('The optimal path for the starting point to go to the end point is:\n', optimal_path[0])
     print('The costs for the optimal path would be:', optimal_costs[0])
 
 
-main()
+testset = [
+    [[4, 0, 8], [-3, -4, 7], [-8, -1, 7]],
+    [[4, 0, 8, -4], [-3, -4, 7, -5], [-8, -1, 7, 1], [-4, -4, 6, 1]]
+           ]
 
+for matrix in testset:
+    main(matrix)
+    print('_' * 180)
